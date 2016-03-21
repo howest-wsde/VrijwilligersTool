2ced9c71b4b42d9fda2dd787f0dc45c5585f8103
thibault durnez <thibault.durnez@student.howest.be>
2016-03-21 15:56:27 +0100

    Vacancies can be printed by exporting them to pdf and opening them in the browser

--- src/AppBundle/Entity/Vacancy.php
+++ src/AppBundle/Entity/Vacancy.php
@@ -301,7 +301,7 @@ class Vacancy
     {
         return $this->skillproficiency;
     }
-
+    
     function __toString()
     {
         return "id: ".$this->getId().
@@ -313,4 +313,10 @@ class Vacancy
         ", organisationId: ".$this->getOrganisationid().$this->getId().
         ", skillId: ".$this->getSkillid().$this->getId();
     }
+
+    public function createPDF(){
+
+    }
+
+
 }
