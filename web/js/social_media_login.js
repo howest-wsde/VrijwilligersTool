8ca898124c0650e2d1fee58056e2aac5b7131579
MKoen <koen.missault@telenet.be>
2016-03-24 09:44:38 +0100

    Google login added (not working on localhost)

--- web/js/social_media_login.js
+++ web/js/social_media_login.js
@@ -1,54 +1,107 @@
-window.fbAsyncInit = function() {
+window.fbAsyncInit = function () {
     FB.init({
-        appId      : '990174991070590', // appId van Koen Missault
-        xfbml      : true,
-        version    : 'v2.5'
+        appId: '990174991070590',
+        xfbml: true,
+        version: 'v2.5'
     });
 };
 
-(function(d, s, id){
+(function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '990174991070590',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.5'
+    });
+
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 function statusChangeCallback(response) {
-    if (response.status === 'connected') {
-        testAPI();
-    }
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '990174991070590',
+            xfbml: true,
+            version: 'v2.5'
+        });
+    };
 }
 
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-}
-
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '990174991070590', // appId van Koen Missault
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v2.5'
-    });
-
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-};
-
-(function(d, s, id) {
+(function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-function testAPI() {
-    FB.api('/me', 'get', { fields: 'name,email' }, function(response) {
-        alert(response.name + " is succesvol ingelogd! E-mail: " + response.email);
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '990174991070590',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.5'
     });
-}
+
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+};
+
+function testAPI(){
+    FB.api('/me', 'get', {fields: 'name,email, gender'}, function (response) {
+        console.log("Facebook Name: " + respone.name);
+        console.log("Facebook Email: " + respone.email);
+        console.log("Facebook Email: " + respone.gender);
+    });
+};
+
+function onSignIn(googleUser){
+    var profile = googleUser.getBasicProfile();
+    console.log("Google ID: " + profile.getId());
+    console.log('Google Full Name: ' + profile.getName());
+    console.log('Google Given Name: ' + profile.getGivenName());
+    console.log('Google Family Name: ' + profile.getFamilyName());
+    console.log("Google Image URL: " + profile.getImageUrl());
+    console.log("Google Email: " + profile.getEmail());
+};
