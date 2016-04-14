9c610f80a1bd42b755f13c0d23089f1cab8e1c44
Jelle Criel <jelle.criel@gmail.com>
2016-04-14 15:55:19 +0200

    removed blank line
    I'm actually testing my gpg keys :p

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-
 class DefaultController extends Controller
 {
     /**
