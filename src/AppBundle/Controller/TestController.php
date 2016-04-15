332d468a8dd04af2235c3ae1746d64795c7e9e29
Jelle Criel <jelle.criel@gmail.com>
2016-04-15 16:40:53 +0200

    Revert "added tostring to Testimonial"
    This reverts commit da75b5a0fdb98df0ab8968f902df4e75b8448def.

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -18,7 +18,7 @@ class TestController extends Controller
     /**
      * @Route("/test/write", name="test_write")
      */
-    public function test_writeAction()
+    public function TestAction()
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -84,7 +84,6 @@ class TestController extends Controller
             ->setSender($otherperson)
             ->setReceiver($person)
             ->setApproved(true);
-        $em->persist($test);
 
         $em->flush();
 
@@ -94,44 +93,74 @@ class TestController extends Controller
     /**
      * @Route("/test/read", name="test_read")
      */
-    public function test_readAction()
+    public function TestAction()
     {
         $em = $this->getDoctrine()->getManager();
 
-        $person = $this->getDoctrine()
-            ->getRepository("AppBundle:Person")
-            ->findOneById(19);
+        $person = new Person();
+        $person->setFirstname("jelle")
+            ->setLastname("Criel")
+            ->setUsername("snellejelle")
+            ->setPassphrase("veilig?")
+            ->setStreet("Sint-Corneliusstraat")
+            ->setNumber(7)
+            ->setPostalCode(9280)
+            ->setCity("Lebbeke")
+            ->setEmail("jelle.criel@student.howest.be")
+            ->setTelephone("0477459599");
+        $em->persist($person);
 
-        $otherperson = $this->getDoctrine()
-            ->getRepository("AppBundle:Person")
-            ->findOneById(20);
+        $otherperson = new Person();
+        $otherperson->setFirstname("koen")
+            ->setLastname("cornelis")
+            ->setUsername("badlapje")
+            ->setPassphrase("niet vielig?")
+            ->setStreet("eeklo")
+            ->setNumber(8)
+            ->setPostalCode(1234)
+            ->setCity("Gent")
+            ->setEmail("koen.cornelis@student.howest.be")
+            ->setTelephone("0123456789");
+        $em->persist($otherperson);
 
-        $org = $this->getDoctrine()
-            ->getRepository("AppBundle:Organisation")
-            ->findOneById(8);
+        $org = new Organisation();
+        $org->setName("Howest")
+            ->setEmail("howest@howest.be")
+            ->setStreet("Sint-Corneliusstraat")
+            ->setNumber(7)
+            ->setPostalCode(9280)
+            ->setCity("Lebbeke")
+            ->setTelephone("0123456789")
+            ->setDescription("beter dan vives")
+            ->setCreator($person);
+        $em->persist($org);
 
-        $skillparent = $this->getDoctrine()
-            ->getRepository("AppBundle:Skill")
-            ->findOneById(1);
+        $skillparent = new Skill();
+        $skillparent->setName("Computers")
+            ->setParent(null);
+        $em->persist($skillparent);
 
-        $skillchild = $this->getDoctrine()
-            ->getRepository("AppBundle:Skill")
-            ->findOneById(2);
+        $skillchild = new Skill();
+        $skillchild->setname("Programming")
+            ->setParent($skillparent);
+        $em->persist($skillchild);
 
-        $vac = $this->getDoctrine()
-            ->getRepository("AppBundle:Vacancy")
-            ->findOneById(1);
+        $vac = new Vacancy();
+        $vac->setTitle("werk gezocht")
+            ->setDescription("heel leuk en veel werk!")
+            ->setStartdate(new \DateTime())
+            ->setEnddate(new \DateTime())
+            ->setOrganisation($org)
+            ->setSkill($skillparent);
+        $em->persist($vac);
 
-        //
-        $assert_skill = $skillchild->getParent();
-        echo "skill:: ".$skillparent."<br />";
-        echo "assert: ".$assert_skill."<br />";
-        //
+        $test = new Testimonial();
+        $test->setValue("goed gedaan! flinke jongen")
+            ->setSender($otherperson)
+            ->setReceiver($person)
+            ->setApproved(true);
 
-
-        $test = $this->getDoctrine()
-            ->getRepository("AppBundle:Testimonial")
-            ->findOneById(1);
+        $em->flush();
 
         return new Response("done");
     }
