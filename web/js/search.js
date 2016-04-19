babc75988716bf6eb63bdd436e298d0bf071ef2c
Jelle Criel <jelle.criel@gmail.com>
2016-04-19 15:10:48 +0200

    implemented module pattern
    and other remarks

--- web/js/search.js
+++ web/js/search.js
@@ -1,43 +1,44 @@
-$(document).ready(function () {
-    $('#searchq').click(function(){
-        $("#divResult").fadeIn();
+"use strict";
+(function () {
+    var host = "http://localhost:888";
+
+    $(document).ready(function () {
+        $('#searchq').click(function(){
+            $("#divResult").fadeIn();
+        });
+
+        $(document).on("click", function(e) {
+            if (! $(e.target).hasClass("search")) {
+                $("#divResult").fadeOut();
+            }
+        });
+
+        $("#searchq").keyup(search);
     });
 
-    $(document).on("click", function(e) {
-        if (! $(e.target).hasClass("search")){
-            $("#divResult").fadeOut();
-        }
-    });
-});
-
-$(function(){
-    $("#searchq").keyup(function()
-    {
+    var search = function() {
         var searchTerm = $(this).val();
-        if (searchTerm != "")
-        {
-            var host = "http://localhost:888";
-            var url = host + "/api/search";
+        if (searchTerm != "") {
             $.ajax({
                 type: "GET",
-                url: url,
+                url: host + "/api/search",
                 data: "q=" + searchTerm,
                 cache: true,
                 success: searchSucces,
                 error: logError
             });
         }
-    });
-});
+    };
 
-var searchSucces = function(result){
-    $("#divResult").html(result).show();
-    $(".display_box").on("click", function(e){
-        var url = $(e.target).find("#referer").val();
-        window.location.href = "http://localhost:888" + url;
-    });
-};
+    var searchSucces = function(result) {
+        $("#divResult").html(result).show();
+        $(".display_box").on("click", function(e){
+            var url = $(e.target).find("#referer").val();
+            window.location.href = host + url;
+        });
+    };
 
-var logError = function (xhr, message, error) {
-    console.log(xhr, message, error);
-};
+    var logError = function (xhr, message, error) {
+        console.log(xhr, message, error);
+    };
+})();
