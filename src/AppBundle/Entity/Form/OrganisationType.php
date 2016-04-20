f09753dd6afbfd990cf7af2d99dbc0268f315189
Jelle Criel <jelle.criel@gmail.com>
2016-04-20 15:31:22 +0200

    added OrganisationType

---
+++ src/AppBundle/Entity/Form/OrganisationType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppBundle\Entity\Form;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\{
+    ChoiceType,
+    CollectionType,
+    DateType,
+    TextareaType,
+    TextType,
+    SubmitType};
+
+class OrganisationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("name", TextType::class, array(
+                "label" => "organisation.label.name",
+                "translation_domain" => "validators",
+                "attr" => array("placeholder" => "vacancy.label.title")
+            ))
+            ->add("description", TextareaType::class, array(
+                "label" => "vacancy.label.description",
+                "translation_domain" => "validators",
+                "attr" => array("placeholder" => "vacancy.label.description")
+            ))
+            ->add("startdate", DateType::class, array(
+                "label" => "vacancy.label.startdate",
+                "translation_domain" => "validators",
+                "attr" => array("placeholder" => "vacancy.placeholder.date"),
+                "widget" => "single_text"
+            ))
+            ->add("enddate", DateType::class, array(
+                "label" => "vacancy.label.enddate",
+                "translation_domain" => "validators",
+                "attr" => array("placeholder" => "vacancy.placeholder.date"),
+                "widget" => "single_text"
+            ))
+            ->add("submit", SubmitType::class, array(
+                "label" => "vacancy.label.submit",
+                "translation_domain" => "validators",
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            "data_class" => "AppBundle\Entity\Organisation",
+            "csrf_protection" => true,
+            "csrf_field_name" => "_token",
+            // a unique key to help generate the secret token
+            "csrf_token_id"   => "id",
+        ));
+    }
+}
