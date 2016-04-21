72541ece5f4bf932d32a87a83fd2013d5beb1c51
Benedikt Beun <benedikt@beuntje.com>
2016-04-21 16:16:21 +0200

    zoekfuncties ingekort

--- src/AppBundle/SearchResult.php
+++ src/AppBundle/SearchResult.php
@@ -3,12 +3,9 @@
 namespace AppBundle; 
 
 class SearchResult
-{
-    private $title;
-    private $body;
-    private $link;
-    private $type; 
-    private $object; 
+{ 
+    private $class_name; 
+    private $entity; 
 
     const MAX_CHARS = 150;
 
@@ -25,77 +22,22 @@ class SearchResult
     {
         $class_path = get_class($entity);
         $class_name = explode( '\\', $class_path)[2];
-        return SearchResult::{"create".$class_name."Result"}($entity);
+        return new SearchResult($class_name, $entity);
     }
 
-    private static function createPersonResult($vol)
-    { 
-        $title = $vol->getFullName();
-        if ($vol->getUsername())
-        {
-            $title .=" ( ".$vol->getUsername()." )";
-        }
-        $body = substr($vol->getEmail()." some Person body examples"
-            , 0, SearchResult::MAX_CHARS); // skills
-        $link = "/persoon/".$vol->getId(); 
-        $type = "person"; 
-        $object = $vol; 
-        return new SearchResult($title, $body, $link, $type, $object);
-    }
-
-    private static function createOrganisationResult($org)
+    private function __construct($class_name, $entity)
     {
-        $title = $org->getName();
-        $body = substr($org->getDescription()." some organisation body example"
-            , 0, SearchResult::MAX_CHARS);
-        $link = "/vereniging/".$org->getId();
-        $type = "organisation"; 
-        $object = $org; 
-        return new SearchResult($title, $body, $link, $type, $object);
+        $this->class_name = $class_name;
+        $this->entity = $entity;
+    }
+ 
+    public function getClass_name()
+    {
+        return $this->class_name;
     }
 
-    private static function createVacancyResult($vac)
+    public function getEntity()
     {
-        $title = $vac->getTitle();
-        $body = substr($vac->getDescription()." some vacancy body example"
-            , 0, SearchResult::MAX_CHARS);
-        $link = "/vacature/".$vac->getId();
-        $type = "vacancy"; 
-        $object = $vac; 
-        return new SearchResult($title, $body, $link, $type, $object);
-    }
-
-    private function __construct($title, $body, $link, $type, $object)
-    {
-        $this->title = $title;
-        $this->body = $body;
-        $this->link = $link;
-        $this->type = $type;
-        $this->object = $object;
-    }
-
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    public function getBody()
-    {
-        return $this->body;
-    }
-
-    public function getLink()
-    {
-        return $this->link;
-    }
-
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    public function getObject()
-    {
-        return $this->object;
+        return $this->entity;
     }
 }
