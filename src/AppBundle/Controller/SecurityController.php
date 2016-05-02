57c1edaac09a7de07bbd865f498c00bd2603a5d6
Jelle Criel <jelle.criel@gmail.com>
2016-05-02 12:10:04 +0200

    added sapcing

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace AppBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\Person;
 use AppBundle\Entity\Form\UserType;
+
 class SecurityController extends Controller
 {
     /**
