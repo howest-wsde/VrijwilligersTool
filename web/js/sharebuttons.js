eac42ccfb5a74558b03c235a334c652d9aaab3e9
thibault durnez <thibault.durnez@student.howest.be>
2016-05-02 11:29:49 +0200

    marker icons added for home and destination

--- web/js/sharebuttons.js
+++ web/js/sharebuttons.js
@@ -23,7 +23,7 @@ $(function(){
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=129814425951";
+        js.src = "http://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=129814425951";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk');
 
