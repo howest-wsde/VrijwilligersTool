dd2c6cf1190b1e06bc969e9b4953bc0307f2a11d
Jelle Criel <jelle.criel@gmail.com>
2016-05-10 09:59:04 +0200

    refactored validation domain usage

--- src/AppBundle/Entity/Form/PersonType.php
+++ src/AppBundle/Entity/Form/PersonType.php
@@ -22,63 +22,51 @@ class PersonType extends AbstractType
         $builder
             ->add("firstname", TextType::class, array(
                 "label" => "person.label.firstname",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.firstname")
             ))
             ->add("lastname", TextType::class, array(
                 "label" => "person.label.lastname",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.lastname")
             ))
             ->add("username", TextType::class, array(
                 "label" => "person.label.username",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.username")
             ))
             ->add("email", EmailType::class, array(
                 "label" => "person.label.email",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.email")
             ))
             ->add("street", TextType::class, array(
                 "label" => "person.label.street",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.street")
             ))
             ->add("number", NumberType::class, array(
                 "label" => "person.label.number",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.number")
             ))
             ->add("bus", NumberType::class, array(
                 "label" => "person.label.bus",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.bus"),
                 "required" => false
             ))
             ->add("postalcode", NumberType::class, array(
                 "label" => "person.label.postalcode",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.postalcode")
             ))
             ->add("city", TextType::class, array(
                 "label" => "person.label.city",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.city")
             ))
             ->add("telephone", TextType::class, array(
                 "label" => "person.label.telephone",
-                "translation_domain" => "validators",
                 "attr" => array("placeholder" => "person.label.telephone")
             ))
             ->add("linkedinUrl", TextType::class, array(
                 "label" => "person.label.linkedin",
-                "translation_domain" => "validators",
                 "required" => false,
                 "attr" => array("placeholder" => "person.placeholder.linkedin")
             ))
             ->add("plainPassword", RepeatedType::class, array(
-                "translation_domain" => "validators",
                 "type" => PasswordType::class,
                 "first_options"  => array(
                     "label" => "person.label.password",
@@ -90,13 +78,11 @@ class PersonType extends AbstractType
                 ),
             ))
             ->add("termsAccepted", CheckboxType::class, array(
-                "translation_domain" => "validators",
                 "mapped" => false,
                 "constraints" => new IsTrue(),
                 "label" => "person.label.eula"
             ))
             ->add("submit", SubmitType::class, array(
-                "translation_domain" => "validators",
                 "label" => "person.label.submit",
             ));
     }
@@ -104,6 +90,7 @@ class PersonType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
+            "translation_domain" => "validators",
             "data_class" => "AppBundle\Entity\person",
             "csrf_protection" => true,
             "csrf_field_name" => "_token",
