e00f6f451dc18f9d0dfc3e1f01292173f0c5aae8
Koen Missault <MKoen@users.noreply.github.com>
2016-05-17 10:26:34 +0200

    dev_KM Share buttons & mail (#104)
    * Social Media Login (client-sided)  removed
    
    * Facebook Share & Like +  Linkedin Share Added
    
    * English translation added + Dutch translation typos fixed
    
    * Validators updated
    
    * Client-sided username validation added
    
    * Double "Maak een account" link fixed
    
    * javascripts block updated
    
    * Username validation moved to formbuilder (+ console.log removed)
    
    * Regex fixed

--- web/js/sharebuttons.js
+++ web/js/sharebuttons.js
@@ -1,12 +1,23 @@
 "use strict";
 $(function(){
 
+    //Tijdelijke styling
+    $(".share-buttons > div").css({
+        "display":"block",
+        "margin":"5px 0 5px 0"
+    });
+    
     //email
-    $("#email").on("click",()=>{
+    $("#email").on("click",function(){
         var email = "ontvanger@vacature.com";
-        var subject ="Bekijk deze vacature";
-        var message = `Bekijk deze vacature op ${window.location.href}
-                       ${$("article").text()}`;
+        var subject =$("#vacature-title").text() + " - Interessante vacature op Roeselare Vrijwilligt!";
+        //var message = "Bekijk deze vacature op " + window.location.href + "%0D%0A"+ $("article").html();
+        var message = "<strong>" + $("#vacature-title").text() + "</strong>";
+        message += "<p>" + $("#vacature-description").text() + "</p>";
+        message += "<p> Ons aanbod: </p>";
+        message += "<p>" + $("#vacature-organisation").text() + "</p>";
+        message += "<p>" + $("#vacature-location").text() + "</p>";
+        message += "<p> Bekijk online <a href=\"" + window.location.href + "\">hier</a>!";
 
         //todo: mail stylen, maar zal dit maar doen na de overhaul van het designen en fancier maken :)
         var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + message;
@@ -14,19 +25,21 @@ $(function(){
     });
 
     //twitter
-    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+    $('#tweet').attr("data-text", $("#vacature-title").text() + " - Interessante vacature op Roeselare Vrijwilligt!");
+    (function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
         if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
-            fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+            fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs'));
 
     //facebook
-    !function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "http://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=129814425951";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk');
-
-
-
+        $('.fb-like').attr("data-href", document.URL);
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/nl_BE/sdk.js#xfbml=1&version=v2.6&appId=990174991070590";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    
+    //linkedin
+    ('#share-linkedin').attr("data-url", document.URL);
 });
