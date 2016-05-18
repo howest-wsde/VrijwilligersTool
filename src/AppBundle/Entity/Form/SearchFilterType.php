fc9938350755ea0ec8e49dd8c937f83fd2cb3f32
Benedikt Beun <benedikt@beuntje.com>
2016-05-18 12:17:37 +0200

    remove dump

--- src/AppBundle/Entity/Form/SearchFilterType.php
+++ src/AppBundle/Entity/Form/SearchFilterType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType; 
 
 class SearchFilterType extends AbstractType
 {
