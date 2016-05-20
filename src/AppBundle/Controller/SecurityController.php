bc8d8b26a2ed2dfee740b60e62891bf5c416fc5b
Jelle Criel <jelle.criel@gmail.com>
2016-05-20 14:30:05 +0200

    removed todo

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -16,7 +16,6 @@ class SecurityController extends Controller
     */
     public function registerAction(Request $request)
     {
-        //TODO: http://symfony.com/doc/current/cookbook/doctrine/registration_form.html
         $user = new Person();
         $form = $this->createForm(PersonType::class, $user);
         $form->handleRequest($request);
