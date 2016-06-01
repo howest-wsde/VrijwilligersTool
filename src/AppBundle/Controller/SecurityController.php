9f385a35661af017662eacc8b3f0fa18d50d3927
Koen Cornelis <koen.cornelis@vanonderen.be>
2016-06-01 23:16:17 +0200

    loging in now redirects to the page you came from, except if you came from the homepage in which case it goes to the /persoon route

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -44,6 +44,7 @@ class SecurityController extends Controller
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render(
             "security/login.html.twig",
             array(
