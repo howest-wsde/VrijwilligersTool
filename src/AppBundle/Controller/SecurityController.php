77cce9e78efb16274b9c5ce3f50a0f819e2b21fd
Koen Cornelis <koen.cornelis@vanonderen.be>
2016-07-06 13:52:05 +0200

    resolved #210 + refactored login form to be embeddable in other pages & default redirect after login is now frontpage with option to override when embedding the login form

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -60,6 +60,7 @@ class SecurityController extends Controller
                 // last username entered by the user
                 "last_username" => $lastUsername,
                 "error"         => $error,
+                "path"          => false,
             )
         );
     }
