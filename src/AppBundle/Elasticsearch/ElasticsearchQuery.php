63dae57cea657b6fae546d9db3066ef77e32f386
Koen Cornelis <koen.cornelis@vanonderen.be>
2016-07-16 15:25:24 +0200

    added a convenience method to immediately get the entities as a result rather than to get the raw result first.

--- src/AppBundle/Elasticsearch/ElasticsearchQuery.php
+++ src/AppBundle/Elasticsearch/ElasticsearchQuery.php
@@ -52,50 +52,35 @@ class ElasticsearchQuery
         return $this->getEntities();
     }
 
+    /**
+     * Method to convert multiple ES hits into entities from this bundle.  The method uses the getEntities method of an ESMapper instance.  It can handle nested entities.  Hits don't need to be supplied as a param, as they are taken from the raw_result property of this class.  That means this method can only succesfully be called after using the search() method of this class.
+     * Note: it cannot handle values that are an array itself.  Meaning nesting arrays is out of the question.
+     * @return array    An array containing the mapped entities.
+     */
     public function getEntities()
     {
         return $this->esMapper->getEntities($this->raw_result["hits"]["hits"]);
     }
 
     /**
-     * Method to convert multiple ES hits into entities from this bundle.  It can handle nested entities as it checks each value to see whether it is an entity itself (using the isEntity() method from this class).  Hits don't need to be supplied as a param, as they are taken from the raw_result property of this class.  That means this method can only succesfully be called after using the search() method of this class.
-     * Note: it cannot handle values that are an array itself.  Meaning nesting arrays is out of the question.
-     * @return array    An array containing the mapped entities.
-     */
-    public function getEntities()
-    {
-        $entities = array();
-        $hits = $this->raw_result["hits"]["hits"];
-        for ($i=0; $i < count($hits); $i++)
-        {
-            $source = $hits[$i]["_source"];
-            $classname = "AppBundle\Entity\\".ucfirst($hits[$i]["_type"]);
-            $entity = new $classname();
-            foreach ($source as $key => $value) {
-                if (!is_null($value) && !is_array($value))
-                {
-                    if ($this->isEntity($value))
-                    {
-                        $value = $this->jsonToEntity($value);
-                    }
-                    $entity->{"set".$key}($value);
-                }
-            }
-            $id = $hits[$i]["_id"];
-            $entity->setId($id);
-            array_push($entities, $entity);
-        }
-        return $entities;
-    }
-
-    /**
      * A search using the parameters given, this can be a single param or an array of params.
      * @param  array    $params     An array of search parameters
-     * @return json               the raw search result
+     * @return json                 The raw search result
      */
     public function search($params)
     {
         $this->raw_result = $this->client->search($params);
         return $this->raw_result;
+    }
+
+    /**
+     * Convenience method bundling the search and getEntities method together.  Meaning you can pass the search params and get the returned entities as a result.
+     * @param  array    $params     An array of search parameters
+     * @return array                An array containing the mapped entities.
+     */
+    public function searchForEntities($params)
+    {
+        $this->raw_result = $this->client->search($params);
+        return $this->esMapper->getEntities($this->raw_result["hits"]["hits"]);
     }
 }
