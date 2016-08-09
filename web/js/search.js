45a3706076b4635d637fde625b3617f5ca91c011
Koen Cornelis <koen.cornelis@vanonderen.be>
2016-08-09 15:27:50 +0200

    added part of the code for sharing searches

--- web/js/search.js
+++ web/js/search.js
@@ -47,4 +47,53 @@
         //console.log(xhr, message, error);
         $(search["form"]).find(".searchResult").html("<ul><li><span>Er gebeurde een fout tijdens het zoeken. Probeer later opnieuw</span></li></ul>");
     };
+
+    //functionaliteit om een zoekopdracht te delen
+    $("#share_search").on("click", function(){
+        openPopup();
+    });
+
+    $("#search_clipboard").on("click",function(){
+        copyToClipboard(window.location.href);
+        alert("De link staat nu op je klembord!");
+    });
+
+    $("#search_close").on("click",function(){
+        closePopup();
+    });
+
+    var closePopup = function(){
+        $("#light").css("display","none");
+        $('#fade').css("display","none");
+    };
+
+    var openPopup = function(){
+        $("#light").css("display","block");
+        $('#fade').css("display","block");
+        $("#search_url").val(window.location.href);
+    };
+
+    var copyToClipboard = function(text) {
+        if (window.clipboardData && window.clipboardData.setData) {
+            return clipboardData.setData("Text", text);
+        }
+        else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+            var textarea = document.createElement("textarea");
+            textarea.textContent = text;
+            textarea.style.position = "fixed";
+
+            document.body.appendChild(textarea);
+
+            textarea.select();
+
+            try {
+                return document.execCommand("copy");
+            } catch (ex) {
+                console.warn("Copy to clipboard failed.", ex);
+                return false;
+            } finally {
+                document.body.removeChild(textarea);
+            }
+        }
+    }
 })();
