21ed60129714afd3086657ef6c18dfedb7bd7b48
Benedikt Beun <benedikt@beuntje.com>
2016-08-18 16:31:11 +0200

    browser details meesturen met feedback

--- src/AppBundle/Controller/FeedbackController.php
+++ src/AppBundle/Controller/FeedbackController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request; 
+use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Feedback;
 use Symfony\Component\HttpFoundation\Session\Session;
 
@@ -15,24 +15,24 @@ class FeedbackController extends controller
      * @Route("/feedback/post", name="feedback")
      */
     public function feedbackSendAction(Request $request)
-    { 
+    {
 
-        if ($request->isMethod('POST')) {  
-            $feedback = new Feedback(); 
+        if ($request->isMethod('POST')) {
+            $feedback = new Feedback();
             $user = $this->getUser();
-            $feedback->setReporter( $user); 
-            $feedback->setUrl($request->request->get("url")); 
-            $feedback->setReport($request->request->get("body")); 
-            $feedback->setState(Feedback::REPORTED);  
+            $feedback->setReporter( $user);
+            $feedback->setUrl($request->request->get("url"));
+            $feedback->setReport($request->request->get("body") . "\n\n" . $_SERVER['REMOTE_ADDR'] . "\n\n" . $_SERVER['HTTP_USER_AGENT']);
+            $feedback->setState(Feedback::REPORTED);
             $feedback->setReportdate(new \DateTime("now"));
-            $em = $this->getDoctrine()->getManager(); 
+            $em = $this->getDoctrine()->getManager();
             $em->persist($feedback);
             $em->flush();
 
         }
 
        return $this->redirect($request->server->get('HTTP_REFERER'));
-        
+
     }
- 
+
 }
