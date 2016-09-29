9946ef0e8869d8c02f680aee5fb71f19c050f43e
Benedikt Beun <benedikt@beuntje.com>
2016-09-29 14:46:54 +0200

    test

--- src/AppBundle/Entity/Testanswer.php
+++ src/AppBundle/Entity/Testanswer.php
@@ -23,6 +23,11 @@ class Testanswer
     private $answer;
 
     /**
+     * @var string
+     */
+    private $history;
+
+    /**
      * @var integer
      */
     private $weight;
@@ -41,6 +46,30 @@ class Testanswer
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set history
+     *
+     * @param string $history
+     *
+     * @return Testanswer
+     */
+    public function setHistory($history)
+    {
+        $this->history = $history;
+
+        return $this;
+    }
+
+    /**
+     * Get history
+     *
+     * @return string
+     */
+    public function getHistory()
+    {
+        return $this->history;
     }
 
     /**
@@ -137,5 +166,150 @@ class Testanswer
     public function getSkill()
     {
         return $this->skill;
+    }
+    /**
+     * @var integer
+     */
+    private $type1score;
+
+    /**
+     * @var integer
+     */
+    private $type2score;
+
+    /**
+     * @var integer
+     */
+    private $type3score;
+
+    /**
+     * @var integer
+     */
+    private $type4score;
+
+
+    /**
+     * Set type1score
+     *
+     * @param integer $type1score
+     *
+     * @return Testanswer
+     */
+    public function setType1score($type1score)
+    {
+        $this->type1score = $type1score;
+
+        return $this;
+    }
+
+    /**
+     * Get type1score
+     *
+     * @return integer
+     */
+    public function getType1score()
+    {
+        return $this->type1score;
+    }
+
+    /**
+     * Set type2score
+     *
+     * @param integer $type2score
+     *
+     * @return Testanswer
+     */
+    public function setType2score($type2score)
+    {
+        $this->type2score = $type2score;
+
+        return $this;
+    }
+
+    /**
+     * Get type2score
+     *
+     * @return integer
+     */
+    public function getType2score()
+    {
+        return $this->type2score;
+    }
+
+    /**
+     * Set type3score
+     *
+     * @param integer $type3score
+     *
+     * @return Testanswer
+     */
+    public function setType3score($type3score)
+    {
+        $this->type3score = $type3score;
+
+        return $this;
+    }
+
+    /**
+     * Get type3score
+     *
+     * @return integer
+     */
+    public function getType3score()
+    {
+        return $this->type3score;
+    }
+
+    /**
+     * Set type4score
+     *
+     * @param integer $type4score
+     *
+     * @return Testanswer
+     */
+    public function setType4score($type4score)
+    {
+        $this->type4score = $type4score;
+
+        return $this;
+    }
+
+    /**
+     * Get type4score
+     *
+     * @return integer
+     */
+    public function getType4score()
+    {
+        return $this->type4score;
+    }
+    /**
+     * @var integer
+     */
+    private $type5score;
+
+
+    /**
+     * Set type5score
+     *
+     * @param integer $type5score
+     *
+     * @return Testanswer
+     */
+    public function setType5score($type5score)
+    {
+        $this->type5score = $type5score;
+
+        return $this;
+    }
+
+    /**
+     * Get type5score
+     *
+     * @return integer
+     */
+    public function getType5score()
+    {
+        return $this->type5score;
     }
 }
