001e0c8f9f88ee29831b11e17cde897ddce22d77
MKoen <koen.missault@telenet.be>
2016-10-20 10:31:35 +0200

    CandidacyController events refactored and updated

--- src/AppBundle/Controller/CandidacyController.php
+++ src/AppBundle/Controller/CandidacyController.php
@@ -26,80 +26,132 @@ class CandidacyController extends UtilityController
         $candidacy = $repository->findOneById($candidacyId);
         $person = $candidacy->getCandidate();
         $vacancy = $candidacy->getVacancy();
-        $fullname = $person->getFullName();
-        $info = [];
 
         if($action == "approve") { //kandidaat goedkeuren
-            //use reduceByOne method to both subtract one from stillWanted and
-            //close the vacancy if need be, then persist the vacancy
-            $vacancy->reduceByOne();
-            $em->persist($vacancy);
-            $candidacy->setState(Candidacy::APPROVED);
-            $em->persist($candidacy);
-            $em->flush();
-
-            //set digest / send email to all administrators & to candidate
-            $subject = $fullname . ' ' . $t->trans('candidacy.mail.approve');
-            $this->sendDigestAndMail($person, $vacancy, $subject, 'approvedCandidate', DigestEntry::APPROVECANDIDATE);
-            $this->sendDigestAndMail($person, $vacancy, $subject,
-                                'notifyCandidateApproved', false, false, false);
-
-            $this->addFlash('approve_message', $fullname . $t->trans('candidacy.flash.approve') . $vacancy->getTitle() . "."
-            );
+            $this->approveCandidate($person, $candidacy, $vacancy, $em, $t);
         }
         else if($action == "cancel"){ //kandidaat afwijzen
-            $candidacy->setState(Candidacy::DECLINED);
-            $em->persist($candidacy);
-            $em->flush();
-
-            //set digest / send email to all administrators & candidate
-            $subject = $fullname . ' ' . $t->trans('candidacy.mail.disapprove');
-            $this->sendDigestAndMail($person, $vacancy, $subject, 'disapprovedCandidate', DigestEntry::APPROVECANDIDATE, true);
-            $this->sendDigestAndMail($person, $vacancy, $subject,
-                                'notifyCandidateDisapproved', false, false, false);
-
-            $this->addFlash('cancel_message', $fullname .
-                            $t->trans('candidacy.flash.disapprove') .
-                            $vacancy->getTitle() . "."
-                        );
+            $this->rejectCandidate($person, $candidacy, $vacancy, $em, $t);
         }
         else if($action == "remove"){ //actieve vrijwilliger verwijderen
-            $candidacy->setState(Candidacy::REMOVED);
-            $vacancy->increaseByOne();
-            $em->persist($candidacy);
-            $em->persist($vacancy);
-            $em->flush();
-
-            //set digest / send email to all administrators & volunteer
-            $subject = $fullname . ' ' . $t->trans('candidacy.mail.remove');
-            $this->sendDigestAndMail($person, $vacancy, $subject, 'removedVolunteer', DigestEntry::REMOVECANDIDATE);
-            $this->sendDigestAndMail($person, $vacancy, $subject,
-                                'notifyVolunteerRemoved', false, false, false);
-
-
-            $this->addFlash('cancel_message', $fullname .
-                            $t->trans('candidacy.flash.remove') .
-                            $vacancy->getTitle() . "."
-                        );
+            $this->removeCandidate($person, $candidacy, $vacancy, $em, $t);
         }
 
         return $this->redirect($request->headers->get('referer')); //return to sender -Elvis Presley, 1962
     }
 
-    private function sendDigestAndMail($person, $vacancy, $subject, $template, $event, $sent = false, $digest = true){
+    private function approveCandidate($person, $candidacy, $vacancy, $em, $t){
+        //use reduceByOne method to both subtract one from stillWanted and
+        //close the vacancy if need be, then persist the vacancy
+        $vacancy->reduceByOne();
+        $em->persist($vacancy);
+        $candidacy->setState(Candidacy::APPROVED);
+        $em->persist($candidacy);
+        $em->flush();
+
+        $mailInfo = [
+            "candidate" => [
+                "subject" => $t->trans('candidacy.candidate.mail.approve'),
+                "template" => "notifyVolunteerApproved"
+            ],
+            "admins" => [
+                "subject" => $t->trans('candidacy.mail.approve'),
+                "template" => "approvedVolunteer"
+            ]
+        ];
+
+        $this->handleDigestsAndMail($person, $vacancy, $mailInfo, DigestEntry::APPROVECANDIDATE);
+
+        $this->addFlash('approve_message', $person->getFullName() . $t->trans('candidacy.flash.approve') . $vacancy->getTitle() . "."
+        );
+    }
+
+    private function rejectCandidate($person, $candidacy, $vacancy, $em, $t){
+        $candidacy->setState(Candidacy::DECLINED);
+        $em->persist($candidacy);
+        $em->flush();
+
+        $mailInfo = [
+            "candidate" => [
+                "subject" => $t->trans('candidacy.candidate.mail.disapprove'),
+                "template" => "notifyVolunteerDisapproved"
+            ],
+            "admins" => [
+                "subject" => $t->trans('candidacy.mail.disapprove'),
+                "template" => "disapprovedVolunteer"
+            ]
+        ];
+
+        $this->handleDigestsAndMail($person, $vacancy, $mailInfo, DigestEntry::DISAPPROVECANDIDATE);
+
+        $this->addFlash('cancel_message', $person->getFullName() .
+            $t->trans('candidacy.flash.disapprove') .
+            $vacancy->getTitle() . "."
+        );
+    }
+
+    private function removeCandidate($person, $candidacy, $vacancy, $em, $t){
+        $candidacy->setState(Candidacy::REMOVED);
+        $vacancy->increaseByOne();
+        $em->persist($candidacy);
+        $em->persist($vacancy);
+        $em->flush();
+
+        $mailInfo = [
+            "candidate" => [
+                "subject" => $t->trans('candidacy.candidate.mail.remove'),
+                "template" => "notifyVolunteerRemoved"
+            ],
+            "admins" => [
+                "subject" => $t->trans('candidacy.mail.remove'),
+                "template" => "removedVolunteer"
+            ]
+        ];
+
+        $this->handleDigestsAndMail($person, $vacancy, $mailInfo, DigestEntry::REMOVECANDIDATE);
+
+        $this->addFlash('cancel_message', $person->getFullName() .
+            $t->trans('candidacy.flash.remove') .
+            $vacancy->getTitle() . "."
+        );
+    }
+
+    private function handleDigestsAndMail($candidate, $vacancy, $mailInfo, $event){
+        $this->handleDigestsAndMailForCandidate($candidate, $vacancy, $mailInfo["candidate"]["subject"], $mailInfo["candidate"]["template"], $event);
+        $this->handleDigestsAndMailForAdmins($candidate, $vacancy, $mailInfo["admins"]["subject"], $mailInfo["admins"]["template"], $event);
+    }
+
+    private function handleDigestsAndMailForCandidate($candidate, $vacancy, $subject, $template, $event){
         $info = [
             'subject' => $subject,
             'template' => $template . '.html.twig',
             'txt/plain' => $template . '.txt.twig',
             'event' => $event,
-            'sent' => $sent,
+            'isForCandidate' => true,
             'data' => array(
-                'candidate' => $person,
+                'candidate' => $candidate,
                 'org' => $vacancy->getOrganisation(),
                 'vacancy' => $vacancy,
             )
         ];
 
-        $digest ? $this->digestAndMail($info) : $this->sendMail($person, $info);
+        $this->digestAndMail($info);
+    }
+
+    private function handleDigestsAndMailForAdmins($candidate, $vacancy, $subject, $template, $event){
+        $info = [
+            'subject' => $subject,
+            'template' => $template . '.html.twig',
+            'txt/plain' => $template . '.txt.twig',
+            'event' => $event,
+            'isForCandidate' => false,
+            'data' => array(
+                'candidate' => $candidate,
+                'org' => $vacancy->getOrganisation(),
+                'vacancy' => $vacancy,
+            )
+        ];
+
+        $this->digestAndMail($info);
     }
 }
