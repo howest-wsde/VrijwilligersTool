0b0b5e519b72a934d1ab9f117bdac9c638b55cb6
thibault durnez <thibault.durnez@student.howest.be>
2016-11-08 12:23:48 +0100

    De marker werd dragable gemaakt en het adres wordt upgedatet volgens het adres waar de marker naartoe werd gesleept, alsook de map werd een height van 300px toegekend om beter te kunnen draggen.

--- web/js/livemap.js
+++ web/js/livemap.js
@@ -25,12 +25,14 @@ var codeAddress = function() {
             marker = new google.maps.Marker({
                 map: map,
                 position: results[0].geometry.location,
+                icon: '/images/homeIcon.png',
                 draggable: true
             });
+
             google.maps.event.addListener(marker, "dragend", function () {
-                document.getElementById('lat').value = marker.getPosition().lat();
-                document.getElementById('lng').value = marker.getPosition().lng();
+                geocodePosition(marker);
             });
+
             if (document.getElementById('lat') !== null) {
                 document.getElementById('lat').value = marker.getPosition().lat();
                 document.getElementById('lng').value = marker.getPosition().lng();
@@ -40,9 +42,30 @@ var codeAddress = function() {
         }
     });
 
+    var geocodePosition = function(marker) {
+        let lat = marker.getPosition().lat();
+        let lng = marker.getPosition().lng();
+
+        geocoder.geocode({'location': {lat: parseFloat(lat), lng: parseFloat(lng)} }, function(results, status) {
+            if (status === 'OK') {
+                if (results[1]) {
+                    $("#edit_person_number").val(results[0].address_components[0].long_name);
+                    $("#edit_person_street").val(results[0].address_components[1].long_name);
+                    $("#edit_person_city").val(results[0].address_components[2].long_name);
+                    $("#edit_person_postalcode").val(results[0].address_components[6].long_name);
+                } else {
+                    console.log('No results found');
+                }
+            } else {
+                console.log('Geocoder failed due to: ' + status);
+            }
+        });
+    }
 };
 
+
 $(document).ready(function(){
+
     geocoder = new google.maps.Geocoder();
     codeAddress();
     $("#address").on('change',function(){codeAddress();});
